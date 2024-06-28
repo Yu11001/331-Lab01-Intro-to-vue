85e045d9f5197ebdb316df8f69229f0671d437f6
Ruth <hyangdan0715@gmail.com>
2024-06-28 09:48:10 +0700

    done till Q5.5

--- main.js
+++ main.js
@@ -3,9 +3,40 @@ const { createApp, ref } = Vue
 
 createApp({
     setup(){
-        const product = ref('Socks')
+        // const product = ref('Socks')
+        const product = ref('Boots')
+        const description = ref('Boots is a cosmetics shop')
+        const image = ref('./assets/images/socks_green.jpg')
+        const productName = ref('http://www.camt.cmu.ac.th')
+        const inStock = ref(false)
+        const inventory = ref(10)
+        const onSale = ref(true)
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ])
+
+        const variants = ref([
+            {id: 2234, color:'green'},
+            {id: 2235, color:'blue'}
+        ])
+        const sizes = ref([
+            'S',
+            'M',
+            'L'
+        ])
         return{
-            product
+            product,
+            description,
+            image,
+            productName,
+            inStock,
+            inventory,
+            onSale,
+            details,
+            variants,
+            sizes
         }
     }
 }).mount('#app')
